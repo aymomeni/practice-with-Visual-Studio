60ad145aa0ef1796d6b6c090ab064ca6c546e2b3
Ali Momeni <aymomeni@gmail.com>
2019-04-25 16:48:43 -0600

    commit of finished chapter

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Working_with_VS.Models;
+using System.Linq;
+
+namespace Working_with_VS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products
+            .Where(p=>p?.Price<50)); 
+            //=> View(SimpleRepository.SharedRepository.Products);
+            //=> View("Index2");
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Working_with_VS.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
