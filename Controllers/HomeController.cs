2a0c6caa566be20a922ab95082469584991b7427
aymomeni <aymomeni@gmail.com>
2019-04-25 20:21:17 -0600

    progressing in chapter 7

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,9 +7,21 @@ namespace Working_with_VS.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
         public IActionResult Index() => View(SimpleRepository.SharedRepository.Products
-            .Where(p=>p?.Price<50)); 
-            //=> View(SimpleRepository.SharedRepository.Products);
-            //=> View("Index2");
+            .Where(p=>p?.Price<50));
+        //=> View(SimpleRepository.SharedRepository.Products);
+        //=> View("Index2");
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
